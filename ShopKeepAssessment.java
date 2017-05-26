558ff4f567742849fc59fa22ddc048430be11854
Philip Limitone <plimitone@gmail.com>
2017-05-26 13:22:38 -0400

    mid day check in
    mid day check in.

--- ShopKeepAssessment.java
+++ ShopKeepAssessment.java
@@ -12,9 +12,8 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
-
 import static org.junit.Assert.*;
-
+import org.openqa.selenium.Keys;
 
 public class ShopKeepAssessment {
 	 
@@ -92,9 +91,7 @@ public class ShopKeepAssessment {
 		Thread.sleep(10000);
 		
 		//Click Login button
-		//ffd.findElement(By.xpath("/html/body/div[2]/div[1]/form/div[4]/input")).click();
-		//ffd.findElement(By.name("commit")).click();
-		//System.out.println("Clicked login....");
+		//Had to find this by NAME because xpath was not working....
 		try {
 			assertEquals("commit", ffd.findElement(By.name("commit")).getAttribute("name"));
 			System.out.println("Found the login button");
@@ -110,23 +107,23 @@ public class ShopKeepAssessment {
 			System.out.println("Something happened, couldn't click the login button");
 		}
 		
-		//click shopkeep back office on top left (from layout screen)
+		//click shopkeep back office on top left (from layout screen) (added this because of browser weirdness on my end)
 		ffd.findElement(By.xpath("/html/body/div[1]/aside/nav/div/a[2]/img")).click();
 		System.out.println("Clicked Back office at top left to go to the bird's eye view screen");
 		
-		//now at bird's eye view main page, select Ipad Button Layout from Items
+		//now at bird's eye view main page, select Ipad Button Layout from Items (added this because of browser weirdness on my end)
 		ffd.findElement(By.xpath("/html/body/div[1]/aside/nav/ul/li[3]/a")).click();
 		System.out.println("Click to expand items list on Bird's Eye View screen");
 		
-		//now click ipad button layout from the expanded items list
+		//now click ipad button layout from the expanded items list (added this because of browser weirdness on my end)
 		ffd.findElement(By.xpath("/html/body/div[1]/aside/nav/ul/li[3]/ul/li[2]/a")).click();
 		
 		//Verify that we're on the iPad Button Layout screen
 		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[1]/h1"));
 		System.out.println("Found ipad Button Layout at the top of the screen");
 
-		try {
 		//Verify the items in the list
+		try {
 		//Bagel
 		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[1]"));
 		System.out.println("Found Bagel");
@@ -173,7 +170,7 @@ public class ShopKeepAssessment {
 		///Now search for an item (one letter)
 		//enter a letter to search in the search field
 		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[1]/input")).sendKeys("a");
-		System.out.println("Clicked on the search field");
+		System.out.println("Clicked on the search field and entered letter");
 
 		//now verify the results in the list
 		try {
@@ -192,10 +189,174 @@ public class ShopKeepAssessment {
 			//Misc Taxable
 			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[8]/span[1]"));
 			System.out.println("Found all the search results items");
-		} catch (Throwable e) {
+			} catch (Throwable e) {
 			System.out.println("Couldn't find search results item(s)");
-		}
+			}
 		////////
+		
+		
+		//Search multiple letters
+		//enter multiple letters to search in the search field
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[1]/input")).sendKeys("on");
+		System.out.println("Clicked on the search field and entered multiple letters");
+		
+		//then verify the results in the list
+		try {
+			//Coffee beans
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[3]/span[2]"));
+			
+			//Donut
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[4]/span[2]"));
+			
+			//Misc Non-taxable
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[7]/span[1]"));
+			System.out.println("Found all the search results items");
+			
+			} catch (Throwable e) {
+			System.out.println("Couldn't find search results item(s)");
+			}
+		/////////
+		
+		//Search one word
+		//first clear the text box of previous entries
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[1]/input")).clear();
+		
+		//then enter a search word (coffee)
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[1]/input")).sendKeys("coffee");
+		
+		//then verify the results
+		//then verify the results in the list
+		try {
+			//Coffee
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[2]/span[2]"));
+			
+			//Coffee Beans
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[3]/span[2]"));
+			System.out.println("Found the results in the word search list");
+			} catch (Throwable e) {
+			System.out.println("Couldn't find search results item(s) in the word search list");
+			}
+		
+		//Clear out the text field for the next test
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[1]/input")).clear();
+		/////////
+		
+		
+		//Rename the existing page
+		//Check that the existing page is there
+		try {
+			//Find New Page
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[4]/div[2]/div[2]/a/span[1]"));
+			System.out.println("Found new page at the bottom");
+			} catch (Throwable e) {
+			System.out.println("Couldn't find new page at the bottom");
+			}
+		
+		//click then Clear the new page field
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[2]/input")).click();
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[2]/input")).clear();
+		System.out.println("cleared the new page field");
+		
+		//Then rename the existing page
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[2]/input")).sendKeys("Phil New Page");
+		//Then press Enter to execute the change
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[2]/input")).sendKeys(Keys.RETURN);
+		System.out.println("Renamed the existing page to Phil New Page");
+		
+		//Check for the renamed page
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[4]/div[2]/div[2]/a/span[1]"));
+		System.out.println("Found the renamed page");
+		///////////////
+		
+		//Create a new page
+		//Click the new page button
+		try {
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[2]/a[1]")).click();
+		System.out.println("Clicked the new page button");
+		} catch (Throwable e) {
+			System.out.println("Did Not click the new page button");
+		}
+		
+		//Verify the new button for the new page is at the bottom (New Page)
+		try {
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[4]/div[2]/div[2]/a[2]/span[1]"));
+		System.out.println("Found the new button");
+		} catch (Throwable e) {
+			System.out.println("couldn't find the new button");
+		}
+		
+		//verify the items in the list on a new page
+		try {
+			//bagel
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[1]"));
+			
+			//coffee
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[2]"));
+			
+			//coffee beans
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[3]"));
+			
+			//donut
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[4]"));
+			
+			//french press
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[5]/span[1]"));
+			
+			//latte
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[6]/span[2]"));
+			
+			//misc non taxable
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[8]/span[2]"));
+			
+			//muffin
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[9]"));
+			
+			//novelty mug
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[10]/span[1]"));
+			
+			//shipping
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[11]"));
+			
+			System.out.println("found all the list items on the new created page");
+		} catch (Throwable e) {
+			System.out.println("Couldn't find items on the list on the new created page");
+		}
+		///////////
+		
+		//delete the created page
+		//first check that it's there
+		try {
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[4]/div[2]/div[2]/a[2]/span[1]"));
+			System.out.println("Found the created page");
+		}catch  (Throwable e) {
+			System.out.println("Couldn't find the created page");
+		}
+		
+		//click on the created page to make sure it's in focus
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[4]/div[2]/div[2]/a[2]/span[1]")).click();
+		
+		//then click the delete button on top right
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[2]/a[2]")).click();
+		
+		//verify the confirmation dialog appears, check the title to be sure
+		ffd.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div[1]/h2"));
+		System.out.println("found the title of the remove dialog");
+		
+		//Wait to make sure things catch up
+		System.out.println("wait a bit to catch up...");
+		Thread.sleep(5000);
+		
+		//Click confirm button
+		ffd.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/div[2]/button[2]")).click();
+		System.out.println("Confirm button clicked");
+		
+		//now check that the button is no longer on the screen (Check that it's  NOT there)
+		try {
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[4]/div[2]/div[2]/a[2]/span[1]")).isDisplayed();
+		System.out.println("button no longer displayed");
+		} catch (Throwable e) {
+			System.out.println(e);
+		}
 		
 		System.out.println("For now we're done....\n");
 		//close the window when all is done
