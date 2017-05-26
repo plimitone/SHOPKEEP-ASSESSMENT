3ecafc7a34e5458f7776fac0367fdef044c9195a
Philip Limitone <plimitone@gmail.com>
2017-05-26 08:20:08 -0400

    check in of morning work so far
    check in of morning work so far

--- ShopKeepAssessment.java
+++ ShopKeepAssessment.java
@@ -79,7 +79,7 @@ public class ShopKeepAssessment {
 		Thread.sleep(5000);
 		
 		//Maximize the webpage
-		//ffd.manage().window().maximize();
+		ffd.manage().window().maximize();
 		
 		//Now login using user name and password
 		ffd.findElement(By.xpath("/html/body/div/div[1]/form/div[2]/div/input")).sendKeys("philip_limitone@shopkeep.com");
@@ -88,12 +88,38 @@ public class ShopKeepAssessment {
 		System.out.println("Entered password");
 		
 		//Wait for things to clear up
-		//System.out.println("Wait for things to clear up before clicking login");
-		//Thread.sleep(10000);
+		System.out.println("Wait for things to clear up before clicking login");
+		Thread.sleep(10000);
 		
 		//Click Login button
-		ffd.findElement(By.xpath("//div[4]/input")).click();
-		System.out.println("Clicked login....");
+		//ffd.findElement(By.xpath("/html/body/div[2]/div[1]/form/div[4]/input")).click();
+		//ffd.findElement(By.name("commit")).click();
+		//System.out.println("Clicked login....");
+		try {
+			assertEquals("commit", ffd.findElement(By.name("commit")).getAttribute("name"));
+			System.out.println("Found the login button");
+		} catch (Throwable e) {
+			System.out.println("Something went wrong, couldn't find the login button");
+		}
+		
+		//Now click the login button
+		try {
+		ffd.findElement(By.name("commit")).click();
+		System.out.println("Clicked the login button");
+		} catch (Throwable e) {
+			System.out.println("Something happened, couldn't click the login button");
+		}
+		
+		//click shopkeep back office on top left (from layout screen)
+		ffd.findElement(By.xpath("/html/body/div[1]/aside/nav/div/a[2]/img")).click();
+		System.out.println("Clicked Back office at top left to go to the bird's eye view screen");
+		
+		//now at bird's eye view main page, select Ipad Button Layout from Items
+		ffd.findElement(By.xpath("/html/body/div[1]/aside/nav/ul/li[3]/a")).click();
+		System.out.println("Click to expand items list on Bird's Eye View screen");
+		
+		//now click ipad button layout from the expanded items list
+		ffd.findElement(By.xpath("/html/body/div[1]/aside/nav/ul/li[3]/ul/li[2]/a")).click();
 		
 		//Verify that we're on the iPad Button Layout screen
 		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[1]/h1"));
@@ -143,6 +169,33 @@ public class ShopKeepAssessment {
 		} catch (Throwable e) {
 			System.out.println("Unable to find an item in the list!!!");
 		}
+		//////
+		///Now search for an item (one letter)
+		//enter a letter to search in the search field
+		ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/header/div[1]/input")).sendKeys("a");
+		System.out.println("Clicked on the search field");
+
+		//now verify the results in the list
+		try {
+			//bagel
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[1]"));
+			
+			//coffee beans
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[3]/span[1]"));
+			
+			//latte
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[6]"));
+			
+			//Misc Non-Taxable
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[7]/span[1]"));
+			
+			//Misc Taxable
+			ffd.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div[2]/div[1]/ul/li[8]/span[1]"));
+			System.out.println("Found all the search results items");
+		} catch (Throwable e) {
+			System.out.println("Couldn't find search results item(s)");
+		}
+		////////
 		
 		System.out.println("For now we're done....\n");
 		//close the window when all is done
